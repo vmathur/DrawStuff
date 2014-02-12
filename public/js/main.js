22afe094a8181172c3b257716c106ffdd1b4a127
Vishal Mathur <vishmathur5@gmail.com>
2014-02-12 14:59:50 -0500

    portrait lock

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,9 @@
 (function (App, Canvas) {
+
+	if (cards.browser && cards.browser.setOrientationLock) {
+		cards.browser.setOrientationLock('portrait');
+	}
+	
 	if(cards.kik && cards.kik.message && cards.kik.message.targetUser){
 		console.log('invited');
 		kik.getUser(function (user) {
